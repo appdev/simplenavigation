fe43789517acb53ba70f59b2b9434f7de3aa3a6e
lengyue <work@run.ci>
2021-11-11 18:13:44 +0800

    init

---
+++ src/api/index.js
@@ -0,0 +1,98 @@
+import axios from 'axios'
+import {getCookie} from "@/utils";
+import * as Qs from "qs";
+
+export const baseURL = 'https://api.start.apkdv.com';
+export const EmptyUrl = 'https://static.apkdv.com/image/web.png!/format/webp/lossless/true';
+
+
+axios.defaults.headers['Content-Type'] = 'application/json';
+axios.defaults.headers.common['token'] = getCookie('token') || '';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = baseURL;
+
+/**
+ * jsonp跨域
+ * @param {String} url [请求地址]
+ * @param {Object} params [请求携带参数]
+ */
+axios.jsonp = (url, params) => {
+    if (!url) {
+        console.error('至少需要一个url参数!')
+        return
+    }
+    return new Promise((resolve) => {
+        window["jQuery33103307611929552594_1612617062297"] = (result) => {
+            resolve(result)
+        }
+        const JSONP = document.createElement('script')
+        JSONP.type = 'text/javascript'
+        JSONP.src = `${url}${Qs.stringify(params)}`
+        document.getElementsByTagName('head')[0].appendChild(JSONP)
+        setTimeout(() => {
+            document.getElementsByTagName('head')[0].removeChild(JSONP)
+        }, 500)
+    })
+}
+
+/**
+ * 接口方法
+ * @param {String} url [请求地址]
+ * @param {Object} params [请求携带参数]
+ */
+export const getJsonp = function (url, params) {
+    return new Promise((resolve, reject) => {
+        axios
+            .jsonp(url, params)
+            .then(res => {
+                resolve(res)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+// 添加请求拦截器，在发送请求之前做些什么(**具体查看axios文档**)--------------------------------------------
+axios.interceptors.request.use(function (config) {
+    // 显示loading
+    return config
+}, function (error) {
+    // 请求错误时弹框提示，或做些其他事
+    return Promise.reject(error)
+})
+
+// 添加响应拦截器(**具体查看axios文档**)----------------------------------------------------------------
+axios.interceptors.response.use(function (res) {
+    return res.data
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error)
+})
+// 封装axios--------------------------------------------------------------------------------------
+export const $get = (url, params) => {
+    return axios.get(url, {params: params})
+}
+
+export const $post = (url, params) => {
+    const options = {
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }
+    return axios.post(url, params, options)
+}
+
+export const $postQs = (url, params) => {
+    const options = {
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }
+    return axios.post(url, Qs.stringify(params), options)
+}
+
+export const $postJS = (url, params) => {
+    const options = {headers: {'Content-Type': 'application/json'}}
+    return axios.post(url, params, options)
+};
+export const $postUp = (url, params) => {
+    const options = {headers: {'Content-Type': 'multipart/form-data'}};
+    return axios.post(url, params, options)
+};
+export default axios
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,266 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-container
+    >
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <img src="@/assets/img/search/Google-icon.svg"
+               v-bind="attrs"
+               v-on="on"
+               width="20px" height="36px"/>
+        </template>
+        <v-list>
+          <v-list-item
+              v-for="(item, index) in searchItems" link :key="index">
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <!-- Google -->
+      <form
+          action="https://www.google.com/search"
+          target="_blank"
+          v-bind:class="{searchbar_form_active: isSearch}"
+          v-if="searchEngine == 'google'"
+          class="searchbar_form"
+      >
+        <v-text-field
+            v-on:click="toSearch"
+            v-bind:class="{searchbar_input_active: isSearch}"
+            type="text"
+            name="q"
+            autocomplete="off"
+            placeholder="Search"
+        ></v-text-field>
+
+      </form>
+
+      <!-- Bing -->
+      <form
+          action="https://www.bing.com/search"
+          v-bind:class="{searchbar_form_active: isSearch}"
+          v-if="searchEngine == 'bing'"
+          class="searchbar_form"
+      >
+        <input
+            v-on:click="toSearch"
+            v-bind:class="{searchbar_input_active: isSearch}"
+            class="mdui-textfield-input searchbar_input"
+            type="text"
+            name="q"
+            autocomplete="off"
+            placeholder="Search"
+        >
+      </form>
+
+      <!-- Baidu -->
+      <form
+          action="https://www.baidu.com/s"
+          v-bind:class="{searchbar_form_active: isSearch}"
+          v-if="searchEngine == 'baidu'"
+          class="searchbar_form"
+      >
+        <input
+            v-on:click="toSearch"
+            v-bind:class="{searchbar_input_active: isSearch}"
+            class="mdui-textfield-input searchbar_input"
+            type="text"
+            name="wd"
+            autocomplete="off"
+            placeholder="Search"
+        >
+      </form>
+
+      <!-- Search button -->
+      <div class="search-button">
+        <img src="@/assets/img/search/search.svg" width="36px" height="36px"/>
+      </div>
+    </v-container>
+<!--    <v-container>-->
+
+
+<!--      <v-divider></v-divider>-->
+<!--      <v-row no-gutters>-->
+<!--        <v-col-->
+<!--            cols="12"-->
+<!--            sm="6"-->
+<!--            md="8"-->
+<!--        >-->
+<!--          <v-card-->
+<!--              class="pa-2"-->
+<!--              outlined-->
+<!--              tile-->
+<!--          >-->
+<!--            .col-12 .col-sm-6 .col-md-8-->
+<!--          </v-card>-->
+<!--        </v-col>-->
+<!--        <v-col-->
+<!--            cols="6"-->
+<!--            md="4"-->
+<!--        >-->
+<!--          <v-card-->
+<!--              class="pa-2"-->
+<!--              outlined-->
+<!--              tile-->
+<!--          >-->
+<!--            .col-6 .col-md-4-->
+<!--          </v-card>-->
+<!--        </v-col>-->
+<!--      </v-row>-->
+<!--      &lt;!&ndash; Footer &ndash;&gt;-->
+<!--      <div class="footer mdui-card mdui-col-xs-12">-->
+<!--        <ul class="social-buttons">-->
+<!--          <li><a href="https://github.com/appdev/opage" target="_blank"><img src="@/assets/img/footer/github.svg" alt=""-->
+<!--                                                                             width="24px" height="24px;"></a></li>-->
+<!--          <li><a href="https://www.apkdv.com/" target="_blank"><img src="@/assets/img/footer/about.svg" alt=""-->
+<!--                                                                    width="24px" height="24px;"></a></li>-->
+<!--        </ul>-->
+
+<!--        <p class="copyright">-->
+<!--          © 2017 <a href="https://www.apkdv.com/" target="_blank">APKDV</a>-->
+<!--        </p>-->
+<!--      </div>-->
+
+<!--    </v-container>-->
   </div>
 </template>
 
 <script>
+import Store from '@/store'
+
+Store.setCookie("se", "google", 365);
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+    searchItems: ["Google", "Baidu", "Bing"],
+    searchEngine: Store.getCookie('se'),
+  }), methods: {
+    useGoogle: function () {
+      this.searchEngine = 'google';
+      Store.setCookie("se", "google", 30);
+    },
+    useBing: function () {
+      this.searchEngine = 'bing';
+      Store.setCookie("se", "bing", 30);
+    },
+    useBaidu: function () {
+      this.searchEngine = 'baidu';
+      Store.setCookie("se", "baidu", 30);
+    }
   }
 }
 </script>
+<style lang="scss">
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.search_switch {
+  margin: 0;
+  text-align: center;
+  width: 40px;
+  height: 36px;
+  background-color: #fff;
+  color: rgba(0, 0, 0, .87);
+  text-overflow: clip;
+  overflow: hidden;
+  cursor: pointer;
 }
-ul {
-  list-style-type: none;
+
+.search_switch_active {
+  width: 0;
+  height: 56px;
+}
+
+#search_engine {
+  width: 100px;
+}
+
+.searchbar {
+  margin-bottom: 10px;
+  transition: .7s;
+}
+
+.searchbar_active {
   padding: 0;
+  margin-top: -56px;
+  transition: .7s;
+  z-index: 2000;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.searchbar_form {
+  width: calc(100% - 40px);
+  margin: 0;
 }
-a {
-  color: #42b983;
+
+.searchbar_form_active {
+  width: 100%;
+}
+
+.searchbar_input {
+  padding: 0;
+  height: 36px;
+  /*background-color: #e9f3fc;*/
+  background-color: #fff;
+  color: #000;
+  border: 0;
+}
+
+.searchbar_input_active {
+  padding: 0 16px;
+  height: 56px;
+}
+
+.search-button {
+  display: none;
 }
 </style>
+
